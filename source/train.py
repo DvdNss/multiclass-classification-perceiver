b346df186b5eb160e5e90d3aeaaabc133d4e7345
DvdNss <private.david.naisse@gmail.com>
2021-12-17 00:32:01 +0100

    added train script

---
+++ source/train.py
@@ -0,0 +1,18 @@
+# coding:utf-8
+"""
+Filename: train.py
+Author: @DvdNss
+
+Created on 12/17/2021
+"""
+import argparse
+
+def main(args=None):
+    """
+    Train language-perceiver given files and arguments.
+
+    :param args:
+    :return:
+    """
+
+    parser = argparse.ArgumentParser()

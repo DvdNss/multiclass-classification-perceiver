1e5e4fc8d2c65a07a623bcc95bb5e6139e85baa1
DvdNss <private.david.naisse@gmail.com>
2021-12-17 14:55:19 +0100

    updated args types

--- source/train.py
+++ source/train.py
@@ -34,9 +34,9 @@ def main():
     parser.add_argument('--train_data', help='Path to train torch file. ', default='data/train.pt')
     parser.add_argument('--validation_data',
                         help='Path to validaton torch file. If not provided, no validation will occur. ', default=None)
-    parser.add_argument('--batch_size', help='Batch size. ', default=1)
+    parser.add_argument('--batch_size', help='Batch size. ', default=1, type=int)
     parser.add_argument('--lr', help='Learning rate. ', default=5e-5)
-    parser.add_argument('--epochs', help='Number of epochs. ', default=1)
+    parser.add_argument('--epochs', help='Number of epochs. ', default=1, type=int)
     parser.add_argument('--output_dir', help='Output directory. ', default='model')
     parser = parser.parse_args()
 
@@ -58,7 +58,7 @@ def main():
     optimizer = AdamW(model.parameters(), lr=parser.lr)
 
     # Train the model
-    for epoch in range(int(parser.epochs)):
+    for epoch in range(parser.epochs):
 
         # Put model in training mode
         model.train()

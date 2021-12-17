f3d51f5f91276eb6a2caacac027fb4431c485f0a
DvdNss <private.david.naisse@gmail.com>
2021-12-17 03:06:19 +0100

    added map functions

--- source/databuilder.py
+++ source/databuilder.py
@@ -13,6 +13,8 @@ import torch
 from datasets import load_dataset, list_datasets
 from transformers import PerceiverTokenizer
 
+from mapping import map_inputs, map_targets
+
 
 def main(args=None):
     """
@@ -27,6 +29,7 @@ def main(args=None):
     parser.add_argument('--split', help="Subsets to use. Separe them by '+' if multiple (ex: train+validation). ",
                         default='train')
     parser.add_argument('--output_dir', help='Output directory. ', default='data')
+    parser.add_argument('--max_size', help='Max size of dataset. ', default=None)
     parser = parser.parse_args(args)
 
     # Load tokenizer
@@ -35,18 +38,56 @@ def main(args=None):
     for subset in parser.split.split('+'):
         # Load dataset
         if parser.dataset in list_datasets():
-            dataset = load_dataset(parser.dataset, split=subset)
+            if parser.max_size is not None:
+                dataset = load_dataset(parser.dataset, split=f"{subset}[0:{parser.max_size}]")
+            else:
+                dataset = load_dataset(parser.dataset, split=subset)
         else:
             # Raise error
             raise FileNotFoundError(f"Couldn't find dataset {parser.dataset} or subset {parser.split}. Please use "
                                     f"list_datasets() to list all available datasets. ")
 
+        # Load labels
+        labels = ["admiration",
+                  "amusement",
+                  "anger",
+                  "annoyance",
+                  "approval",
+                  "caring",
+                  "confusion",
+                  "curiosity",
+                  "desire",
+                  "disappointment",
+                  "disapproval",
+                  "disgust",
+                  "embarrassment",
+                  "excitement",
+                  "fear",
+                  "gratitude",
+                  "grief",
+                  "joy",
+                  "love",
+                  "nervousness",
+                  "optimism",
+                  "pride",
+                  "realization",
+                  "relief",
+                  "remorse",
+                  "sadness",
+                  "surprise",
+                  "neutral"]
+        id2label = {idx: label for idx, label in enumerate(labels)}
+        label2id = {label: idx for idx, label in enumerate(labels)}
+
         # Map inputs
-        formatted_dataset = dataset.map(lambda row: tokenizer(row['text'], padding="max_length", truncation=True),
-                                        batched=True)
+        formatted_dataset = dataset.map(lambda row: tokenizer(map_inputs(row), padding="max_length", truncation=True),
+                                        batched=True, remove_columns=['id', 'text'])
+
+        # Map labels
+        formatted_dataset = formatted_dataset.map(lambda row: map_targets(row['labels']), remove_columns=['labels'])
 
         # Build tensor
-        formatted_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
+        formatted_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'targets'])
 
         # Save the pt file
         if not os.path.exists(parser.output_dir):

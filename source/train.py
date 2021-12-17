c524456f02e779851bc591c8f91d73b37eea50d5
DvdNss <private.david.naisse@gmail.com>
2021-12-17 09:51:40 +0100

    shortened dataloader call, added evaluation, added progress bar and loss/accu/memory logs

--- source/train.py
+++ source/train.py
@@ -6,12 +6,16 @@ Author: @DvdNss
 Created on 12/17/2021
 """
 import argparse
+import os
 
 import torch
+from datasets import load_metric
 from sklearn.metrics import accuracy_score
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from transformers import PerceiverForSequenceClassification, AdamW
+
+from evaluate import evaluate
 
 
 def main(args=None):
@@ -32,14 +36,13 @@ def main(args=None):
     parser.add_argument('--batch_size', help='Batch size. ', default=1)
     parser.add_argument('--lr', help='Learning rate. ', default=5e-5)
     parser.add_argument('--epochs', help='Number of epochs. ', default=1)
+    parser.add_argument('--output_dir', help='Output directory. ', default='model')
     parser = parser.parse_args()
 
     # Call dataloaders
-    train_dataset = torch.load(parser.train_data)
-    train_dataloader = DataLoader(train_dataset, batch_size=parser.batch_size, shuffle=True)
-    if parser.validation_data is not None:
-        validation_dataset = torch.load(parser.validation_data)
-        train_dataloader = DataLoader(validation_dataset, batch_size=parser.batch_size)
+    train_dataloader = DataLoader(torch.load(parser.train_data), batch_size=parser.batch_size, shuffle=True)
+    validation_dataloader = DataLoader(torch.load(parser.validation_data),
+                                       batch_size=parser.batch_size) if parser.validation_data is not None else None
 
     # Load labels
     labels = ["admiration",
@@ -70,45 +73,79 @@ def main(args=None):
               "sadness",
               "surprise",
               "neutral"]
-    id2label = {idx: label for idx, label in enumerate(labels)}
-    label2id = {label: idx for idx, label in enumerate(labels)}
 
     # Load model
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = PerceiverForSequenceClassification.from_pretrained('deepmind/language-perceiver')
+    model = PerceiverForSequenceClassification.from_pretrained('deepmind/language-perceiver',
+                                                               problem_type='multi_label_classification',
+                                                               num_labels=len(labels))
 
     # Send model to device
     model.to(device)
 
-    # Train the model
+    # Define optimizer and metric
     optimizer = AdamW(model.parameters(), lr=parser.lr)
-    model.train()
-    for epoch in range(parser.epochs):
-        print("Epoch: ", epoch)
-        for batch in tqdm(train_dataloader):
-            # Get inputs
-            inputs = batch['input_ids'].to(device)
-            attention_mask = batch['attention_mask'].to(device)
-            targets = batch['targets'].to(device)
+    accuracy = load_metric('accuracy')
 
-            print(inputs, targets)
-            # Zero gradients
-            optimizer.zero_grad()
+    # Train the model
+    for epoch in range(int(parser.epochs)):
+        model.train()
+        accu_logs = []
+        loss_logs = []
+        mem_logs = []
+        with tqdm(train_dataloader, unit='batches') as progression:
+            for batch in progression:
+                progression.set_description(f"Epoch {epoch}")
+                # Get inputs
+                inputs = batch['input_ids'].to(device)
+                attention_mask = batch['attention_mask'].to(device)
+                targets = batch['targets'].type(torch.FloatTensor).to(device)
 
-            # Fwd + bwd + opt
-            outputs = model(inputs=inputs, attention_mask=attention_mask, labels=targets)
-            loss = torch.nn.BCEWithLogitsLoss()(outputs[0], targets)
-            loss.backward()
-            optimizer.step()
+                # Zero gradients
+                optimizer.zero_grad()
 
-            # Evaluate if validation_data specified
-            if parser.validation_data is not None:
-                predictions = outputs.logits.cpu().detach().numpy()
-                accuracy = accuracy_score(y_true=batch["labels"].numpy(), y_pred=predictions)
-                print(f"Loss: {loss.item()}, Accuracy: {accuracy}")
-            else:
-                print(f"Loss: {loss.item()}")
-                print("Skipping validation since no validation data has been provided... ")
+                # Fwd + bwd + opt
+                outputs = model(inputs=inputs, attention_mask=attention_mask, labels=targets)
+                loss = outputs.loss
+                loss.backward()
+                optimizer.step()
+
+                # Evaluate if validation_data specified
+                if parser.validation_data is not None:
+                    # Get predictions and targets
+                    predictions = outputs.logits.cpu().detach().numpy()
+                    references = batch["targets"].numpy()
+
+                    # Binarize predictions
+                    for i, example in enumerate(predictions):
+                        for j, category in enumerate(example):
+                            predictions[i][j] = 1 if category > 0.5 else 0
+
+                    # Retrieve acc and mem
+                    accuracy = accuracy_score(y_true=references, y_pred=predictions)
+                    memory = round(torch.cuda.memory_reserved(device) / 1e9, 2)
+
+                    # Append data to logs
+                    accu_logs.append(accuracy)
+                    loss_logs.append(loss.item())
+                    mem_logs.append(memory)
+                    progression.set_postfix(loss=round(sum(loss_logs) / len(loss_logs), 3),
+                                            accuracy=round(sum(accu_logs) / len(accu_logs) * 100, 1),
+                                            memory=f"{round(sum(mem_logs) / len(mem_logs), 2)}Go")
+                else:
+                    progression.set_postfix(loss=round(sum(loss_logs) / len(loss_logs), 3),
+                                            accuracy='disabled',
+                                            memory=f"{round(sum(mem_logs) / len(mem_logs), 2)}Go")
+
+        # Create output directory if needed
+        if not os.path.exists(parser.output_dir):
+            os.mkdir(parser.output_dir)
+
+        if validation_dataloader is not None:
+            epoch_acc = evaluate(model=model, validation_dataloader=validation_dataloader)
+            torch.save(model, f"{parser.output_dir}/perceiver-e{epoch}-acc{int(epoch_acc)}.pt".replace('//', '/'))
+        else:
+            torch.save(model, f"{parser.output_dir}/perceiver-e{epoch}.pt".replace('//', '/'))
 
 
 if __name__ == '__main__':
